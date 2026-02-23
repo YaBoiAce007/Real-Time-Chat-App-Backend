7aa8229928f491aa716c759083a9ae32c915928e
Aniketh Gurung <anikethg68@gmail.com>
2026-02-23 17:16:19 +0530

    first commit

--- src/main/java/com/real_time_chat_app/real_time_chat_app/Security/JwtService.java
+++ src/main/java/com/real_time_chat_app/real_time_chat_app/Security/JwtService.java
@@ -30,7 +30,7 @@ public class JwtService {
                 .add(customClaims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60))
                 .and()
                 .signWith(getKey())
                 .compact();
